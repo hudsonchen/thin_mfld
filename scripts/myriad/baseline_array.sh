698e3a94ac4a64b4b93e7af5c166157558bce06b
hudsonchen <hudsonchen33@outlook.com>
2025-12-01 10:16:13 +0000

    minor

--- scripts/myriad/baseline_array.sh
+++ scripts/myriad/baseline_array.sh
@@ -4,4 +4,4 @@ jobs_in_parallel=$(wc -l < "$1")
 echo $jobs_in_parallel
 echo $1
 
-qsub -t 1-${jobs_in_parallel} /home/ucabzc9/Scratch/mmd_flow_cubature/scripts/myriad/baseline_base.sh "$1"
+qsub -t 1-${jobs_in_parallel} /home/ucabzc9/Scratch/thinned_mfld/scripts/myriad/baseline_base.sh "$1"
