dd6233d9bc8b4934ec9a0c335e963406ce56e209
hudsonchen <hudsonchen33@outlook.com>
2025-12-01 22:13:38 +0000

    d

--- scripts/gatsby/base.sh
+++ scripts/gatsby/base.sh
@@ -2,7 +2,7 @@
 #SBATCH -p cpu
 #SBATCH --job-name=thinned_mfld
 #SBATCH --time=02:00:00        
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=10G
 #SBATCH --chdir=/nfs/ghome/live/jwornbard/hudson
 #SBATCH --output=thinned_mfld_%A_%a.out
@@ -13,6 +13,8 @@
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 export MKL_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export OPENBLAS_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export NUMEXPR_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 export XLA_FLAGS="--xla_cpu_multi_thread_eigen=true intra_op_parallelism_threads=${SLURM_CPUS_PER_TASK}"
 
 # Get the line corresponding to this array task
